3c447fb734392bee8b032d15adb4752c3b4b4641
Raphael Büchler <raphael.buchler@sulamerica.com.br>
2024-07-04 19:33:07 -0300

    frst commit
    frst commit

---
+++ IDwallToolkit.xcframework/ios-arm64_x86_64-simulator/IDwallToolkit.framework/Headers/IDwallToolkit.h
@@ -0,0 +1,18 @@
+//
+//  IDwallToolkit.h
+//  IDwallToolkit
+//
+//  Created by IDwall on 28/04/18.
+//  Copyright © 2018 IDwall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for IDwallToolkit.
+FOUNDATION_EXPORT double IDwallToolkitVersionNumber;
+
+//! Project version string for IDwallToolkit.
+FOUNDATION_EXPORT const unsigned char IDwallToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDwallToolkit/PublicHeader.h>
